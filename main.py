3e4f794d8a0b9178151b3ccfc4b8423a40503890
YaSanyaBeats <75445623+YaSanyaBeats@users.noreply.github.com>
2022-02-06 21:46:08 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,45 @@
+import tkinter as tk
+from errors import errorCodes, error
+from lab1 import startLab1
+
+def main():
+    window = tk.Tk()
+    window.title("Выч мат от гения")
+    columns = 4
+    rows = 3
+    buttonCommands = [startLab1,
+                      lambda er=errorCodes.LAB_NOT_FINED: error(er),
+                      lambda er=errorCodes.LAB_NOT_FINED: error(er),
+                      lambda er=errorCodes.LAB_NOT_FINED: error(er),
+                      lambda er=errorCodes.LAB_NOT_FINED: error(er),
+                      lambda er=errorCodes.LAB_NOT_FINED: error(er),
+                      lambda er=errorCodes.LAB_NOT_FINED: error(er),
+                      lambda er=errorCodes.LAB_NOT_FINED: error(er),
+                      lambda er=errorCodes.LAB_NOT_FINED: error(er),
+                      lambda er=errorCodes.LAB_NOT_FINED: error(er),
+                      lambda er=errorCodes.LAB_NOT_FINED: error(er),
+                      lambda er=errorCodes.LAB_NOT_FINED: error(er)]
+
+    for i in range(columns):
+        window.columnconfigure(i, weight=1, minsize=75)
+
+        for j in range(rows):
+            window.rowconfigure(j, weight=1, minsize=50)
+            frame = tk.Frame(master=window)
+            frame.grid(row=j, column=i, padx=30, pady=30)
+            number = rows * j + (i + 1)
+            button = tk.Button(
+                master=frame,
+                text="Лабораторная №" + str(number),
+                bg="green",
+                fg="white",
+                height=5,
+                command=buttonCommands[number - 1]
+            )
+            button.pack()
+
+    window.mainloop()
+
+
+if __name__ == '__main__':
+    main()
